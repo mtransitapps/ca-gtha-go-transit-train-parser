1c974ab13005fa3bd70f4dca5563b0f6f6fccd14
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 16:57:01 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_gtha_go_transit_train/GTHAGOTransitTrainAgencyTools.java
+++ src/org/mtransit/parser/ca_gtha_go_transit_train/GTHAGOTransitTrainAgencyTools.java
@@ -40,7 +40,7 @@ public class GTHAGOTransitTrainAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating GO Transit train data...\n");
+		System.out.printf("\nGenerating GO Transit train data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
